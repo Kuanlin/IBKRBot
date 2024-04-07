10b0ab4797b570967f7c642da5b1e820c79dca42
Kuanlin <kuan.lyn.chen@gmail.com>
2024-04-07 11:29:33 +0800

    update

--- botmodel/model.py
+++ botmodel/model.py
@@ -1,6 +1,7 @@
 import asyncio
 from ModelBase import ModelBase
-from restRequest import *
+import RESTRequest
+from pprint import pprint as pp
 
 class MyModel(ModelBase):
 
@@ -8,12 +9,35 @@ class MyModel(ModelBase):
         pass
     
     async def onRestResponse(self, message:dict):
+        print("onRestResponse")
+        pp(message)
+
+    async def portfolioLedger(self):
+        print("portfolioLedger")
+        self.restRequest.send_message(await RESTRequest.portfolioLedger)
+
+    async def modelModifyOrders(self):
+        pass
+
+    async def modelDeleteOrders(self):
+        #insert model order decision to database
+        #dbmsgr.send_messages(..//)
+        #restRequest.send_message([ DeleteOrders ...])
+        #restRequest.send_message([ check live Orders ])
+        pass
+
+    async def modelPlaceOrders(self):
+        #insert model order decision to database
+        #dbmsgr.send_messages(..//)
+        #restRequest.send_message([ PlaceOrders ...])
+        #restRequest.send_message([ check live Orders ])
         pass
 
     async def entry(self):
         print("MyModel Entry")
         self.system.on_message = onSysMessage
         self.restResponse.on_message = onRestResponse
+        self.portfolioLedger()
 
     async def mainloop(self):
         print("MyModel MainLoop")
